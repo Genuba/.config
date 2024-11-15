19fd251839ac18706fa3ab4b0a9bd41b9a2d4daa
Genuba <daalpiva@gmail.com>
2024-11-15 16:48:30 -0500

    disable copilot

--- nvim/lua/acocarip/plugins/copilot.lua
+++ nvim/lua/acocarip/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   "github/copilot.vim",
   config = function()
     -- Example configuration for copilot.vim (you can modify this according to your preferences)
-    vim.g.copilot_no_tab_map = true -- Disable the default tab mapping
+    vim.g.copilot_no_tab_map = false -- Disable the default tab mapping
     vim.g.copilot_assume_mapped = true -- Automatically assume Copilot is ready
     vim.g.copilot_filetypes = { -- Enable Copilot for specific file types
       lua = true,
